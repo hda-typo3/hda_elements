250a79201a7a3538671f5e7c5ac544c3c5dbf387
Michael Lang <115140776+hda-typo3@users.noreply.github.com>
2024-07-03 14:22:42 +0200

    Update pages.php

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -6,3 +6,11 @@ defined('TYPO3_MODE') || defined('TYPO3') || die('Access denied.');
 		'Configuration/TypoScript/Setup', 
 		'HDA-Elements'
 );
+
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+	'hda_elements',
+    	'Configuration/TsConfig/Page/Mod/Wizards/NewContentElement.tsconfig',
+    	'New Elements'
+);
